79240562611601b8da2d9e673be4d6a306a65849
nottx/PC <a.t.blasiak@gmail.com>
2025-04-16 08:16:01 +0200

    Stworzenie podstawowego DFS, nic wielkiego ani użytecznego, fundament pod bardziej skomplikwoane funckje

---
+++ Grafy/Wzorce_algorytmow/DFS.py
@@ -0,0 +1,44 @@
+# Podstawowy, "goły" DFS do modyfikowania na potrzeby zadań
+
+def DFS_visit(G, v):
+    global parent
+    global visited
+    global time
+    visited[v] = True
+    time += 1 # Czas odwiedzenia
+    # Odwiedzanie sąsiadów:
+
+    for n in G[v]:
+        if(visited[n] == False):
+            parent[n] = v
+            DFS_visit(G, n)
+    time += 1 # czas przetworzenia
+
+
+def DFS(G):
+    global visited
+    global parent
+    for v in range(len(G)):
+        if(visited[v] == False):
+            DFS_visit(G, v)
+
+# Graf skierowany:
+adj = [
+    [1, 2],
+    [8],
+    [3, 5],
+    [4],
+    [],
+    [6, 8],
+    [],
+    [],
+    [7]
+]
+
+n = len(adj)
+time = 0
+visited = [False for _ in range(n)]
+parent = [None for _ in range(n)]
+
+DFS(adj)
+
