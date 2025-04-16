d98ba5ea6ccb2398f224020eeedca760312215f1
nottx/PC <a.t.blasiak@gmail.com>
2025-04-16 08:47:22 +0200

    Changed all comments to english

--- Grafy/Wzorce_algorytmow/BFS.py
+++ Grafy/Wzorce_algorytmow/BFS.py
@@ -1,5 +1,5 @@
 """
-Prosty BFS, zapisujący parentów i odległości od wierzchołka globalnie
+Simple BFS, storing parents and distances from the vertex globally
 """
 
 from collections import deque
@@ -8,48 +8,48 @@ def BFS(adj, s=0):
     q = deque()
     put = q.append
     get = q.popleft
-    put(s) # domyślnie 0
+    put(s) # default is 0
     global visited
     global parent
     global d
     while q:
         v = get()
-        # Przeglądanie sąsiednich wierzchołków:
+        # Exploring neighboring vertices:
         for n in adj[v]:
             if(visited[n] == False):
-                # Aktualizacja kolejki
+                # Updating the queue
                 visited[n] = True
                 parent[n] = v
                 d[n] = d[v] + 1
-                # Symulacja postępowania fali do następnego wierzchołka:
+                # Simulating the wave advancing to the next vertex:
                 put(n)
 
 
 
 
-# Zakładamy graf nieskierowany
-# Liczba wierzchołków:
+# Assuming an undirected graph
+# Number of vertices:
 #v = 5
 
-# Lista sąsiedztwa:
+# Adjacency list:
 adj = [[1, 3, 4], [0, 2, 3], [1], [0, 1, 4], [0, 3]]
 adj2 = [
-    [1, 2],        # 0 sąsiaduje z 1 i 2
-    [0, 3, 4, 5],  # 1 sąsiaduje z 0, 3, 4, 5
-    [0, 5],        # 2 sąsiaduje z 0, 5
-    [1],           # 3 sąsiaduje z 1
-    [1, 5, 6],     # 4 sąsiaduje z 1, 5, 6
-    [1, 2, 4, 7],  # 5 sąsiaduje z 1, 2, 4, 7
-    [4, 7],        # 6 sąsiaduje z 4, 7
-    [5, 6]         # 7 sąsiaduje z 5, 6
+    [1, 2],        # 0 is adjacent to 1 and 2
+    [0, 3, 4, 5],  # 1 is adjacent to 0, 3, 4, 5
+    [0, 5],        # 2 is adjacent to 0, 5
+    [1],           # 3 is adjacent to 1
+    [1, 5, 6],     # 4 is adjacent to 1, 5, 6
+    [1, 2, 4, 7],  # 5 is adjacent to 1, 2, 4, 7
+    [4, 7],        # 6 is adjacent to 4, 7
+    [5, 6]         # 7 is adjacent to 5, 6
 ]
 n = len(adj2)
 visited = [False for _ in range(n)]
-d = [0 for _ in range(n)] # odległość
+d = [0 for _ in range(n)] # distance
 parent = [None for _ in range(n)]
 
 BFS(adj2)
-# Próbne znalezienie najkrótszej ścieżki od 0 do 7:
+# Trial: finding the shortest path from 0 to 7:
 v = 7
 print(v, end="")
 while(v != 0):
