5208a4aa165a256de8102d20b68debe032461f91
Nottx/Laptok <a.t.blasiak@gmail.com>
2025-04-18 15:56:33 +0200

    Fixed problems, I was overwriting proc_t[] array in 2nd phase of DFS. Now there are two separate functions for 1st and 2nd phase

--- Grafy/Wzorce_algorytmow/Strong_components.py
+++ Grafy/Wzorce_algorytmow/Strong_components.py
@@ -2,20 +2,17 @@
 # Def.: Two vertexes (u and v) are strongly connected <=>
 #       There is a directed path from u to v and from v to u
 
-def DFS_visit(G, v, final=False):
-    if(final == True):
-        global comp
-        comp.append(v)
-    global parent
+
+# 1st DFS phase:
+def DFS_visit(G, v):
     global visited
     global time
     global proc_t
     visited[v] = True
-    #time += 1 # Visit time
     # Visiting neighbours:
     for n in G[v]:
         if(visited[n] == False):
-            DFS_visit(G, n, final)
+            DFS_visit(G, n)
     proc_t[len(G)-1-time] = v
     time += 1 # Process time
 
@@ -26,23 +23,36 @@ def DFS(G):
         if(visited[v] == False):
             DFS_visit(G, v)
 
-def DFS_by_time(G):
-    global visited
-    global comp
-    for v in proc_t:
-        if(visited[v] == False):
-            DFS_visit(G, v ,True)
-            if(len(comp) > 1): # Added this to avoid considering single vertexes as connected components
-                print(comp)
-            comp = []
-
+# Middle DFS phase:
 def rev_edges(G):
-    global n
+    n = len(G)
     rev = [[] for _ in range(n)]
     for v in range(n):
         for e in G[v]:
             rev[e].append(v)
     return rev
+
+# 2nd FDS phase:
+def DFS_run_components(G, v):
+    global visited
+    global comp
+    comp.append(v)
+    visited[v] = True
+    # Visiting neighbours:
+    for n in G[v]:
+        if(visited[n] == False):
+            DFS_run_components(G, n)
+
+def DFS_by_time(G):
+    global visited
+    global comp
+    for v in proc_t:
+        if(visited[v] == False):
+            DFS_run_components(G, v)
+            if(len(comp) > 1): # Added this to avoid considering single vertexes as connected components
+                print(comp)
+            comp = []
+
 
 # Directed graph:
 adj2 = [
@@ -83,7 +93,7 @@ comp = []
 
 # The main Algorithm:
 DFS(adj)
-print(proc_t)
+#print(proc_t)
 for i in range(n):
     visited[i] = False
 
