c6afd4aaf97ab7025bfbab6169c909427a4e8ef6
Nottx/Laptok <a.t.blasiak@gmail.com>
2025-04-29 18:15:37 +0200

    Init push, hot typed notes during lesson

--- Grafy/Wzorce_algorytmow/Dijkstra.py
+++ Grafy/Wzorce_algorytmow/Dijkstra.py
@@ -1,11 +1,23 @@
 from collections import deque
 
 def Dijkstra(G, s):
-    # Init:
+    # ----------------- INIT -----------------
+    # Queue:
     q = deque()
     put = q.append
     get = q.popleft
     put(s)
 
     n = len(G)
-    d = [float("inf") for _ in range(n)] # Distance from s to vertex
+
+    # Arrays:
+    d = [float("inf") for _ in range(n)] # Distance from s to all vertexes
+    d[s] = 0
+    visited = [False for _ in range(n)]
+    visited[s] = True
+    put(s)
+
+    # -------------- MAIN LOOP --------------
+    while(True)
+
+
---
+++ Grafy/notes/Find_shortes_cycle.py
@@ -0,0 +1,7 @@
+def directed_graph(G):
+    """
+    We count distance between every two vertexes using Floyd-Warshall.
+    Then we count cycle length = d(v, u) + d(u, v) and choose minimal
+    """
+
+def undirected_graph(G):
