a80152fc05c82da8131c064935d54440b49e5f2c
Nottx/Laptok <a.t.blasiak@gmail.com>
2025-05-01 13:20:09 +0200

    Finished and made optimalozations

--- Grafy/Wzorce_algorytmow/Dijkstra.py
+++ Grafy/Wzorce_algorytmow/Dijkstra.py
@@ -1,23 +1,62 @@
-from collections import deque
+from queue import PriorityQueue
 
-def Dijkstra(G, s):
-    # ----------------- INIT -----------------
-    # Queue:
-    q = deque()
-    put = q.append
-    get = q.popleft
-    put(s)
-
+def dijkstra(G, s=0):
+    # Initialization:
     n = len(G)
-
-    # Arrays:
-    d = [float("inf") for _ in range(n)] # Distance from s to all vertexes
-    d[s] = 0
+    parent = [None for _ in range(n)]
+    d = [float("inf") for _ in range(n)]
+    d[s] = 0 # <--- Very important, without this the algorithm won't start
     visited = [False for _ in range(n)]
-    visited[s] = True
-    put(s)
+    pq = PriorityQueue()
+    pq.put((d[s], s))
 
-    # -------------- MAIN LOOP --------------
-    while(True)
+    # Main loop:
 
 
+    """ Slower version:
+    while(True):
+        vertex = -1
+        nearest = float("inf")
+        for i in range(n):
+            # Looking for nearest to s, not yet visited vertex:
+            if(d[i] < nearest and visited[i] == False):
+                nearest = d[i]
+                vertex = i    
+                
+        if(vertex == -1):
+            return parent # Because there are no more vertexes to
+        # Now we know that the vertex exists and we can visit it
+        visited[vertex] = True
+    """
+
+    # Faster version:
+    while(not pq.empty()):
+        prio, vertex = pq.get()
+        if(prio <= d[vertex]): # We check the vertex only if we haven't already found a better path
+            visited[vertex] = True
+            # Relaxation for the newly found vertex:
+            for (v, length) in G[vertex]:
+                if(d[v] > d[vertex] + length and visited[v] == False):
+                    d[v] = d[vertex] + length
+                    parent[v] = vertex
+                    pq.put((d[v], v))
+    return parent
+
+inf = float("inf")
+G=[
+    [(1, 2), (4, 2)],
+    [(0, 2), (2, 3), (4, 1), (5, 8)],
+    [(1, 3), (3, 1)],
+    [(2, 1), (4, 5), (5, 1)],
+    [(0, 2), (1, 1), (3, 5)],
+    [(1, 8), (3, 1)]
+]
+
+start = 0
+end = 5
+path = dijkstra(G, start)
+
+while(end != start):
+    print(end, end=" <--- ")
+    end = path[end]
+print(end)
