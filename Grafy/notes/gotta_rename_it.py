44909cb50474ff8407def854a1d9805bb0266b0b
Nottx/Laptok <a.t.blasiak@gmail.com>
2025-05-02 17:57:48 +0200

    Created algorithm for task written down during lesson

--- Grafy/notes/gotta_rename_it.py
+++
@@ -1,5 +0,0 @@
-# Task: find a path from a to b, that goes only on
-# edges with descending weights
-
-# Idea: we can use Djikstra with some modification in relaxation() function
-# Sort all edges by length, do relaxation, go for it
