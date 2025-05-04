6951bce9814c3acdeff6020673d92e68de3aede5
nottx/PC <a.t.blasiak@gmail.com>
2025-05-04 15:46:39 +0200

    Fixed a typo

--- Graphs/notes/Descending_path.py
+++ Graphs/notes/Descending_path.py
@@ -1,7 +1,7 @@
 # Task: find a path from a to b, that goes only on
 # edges with descending weights
 
-# Idea: we can use Djikstra with some modification in relaxation() function
+# Idea: we can use Dijkstra with some modification in relaxation() function
 # Sort all edges by length, do relaxation, go for it
 
 from queue import PriorityQueue
