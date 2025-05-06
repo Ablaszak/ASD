b5d5030b18964857936a6d4a80cd3c3ca0bcbfa9
nottx/PC <a.t.blasiak@gmail.com>
2025-05-06 16:01:47 +0200

    Solved

--- Graphs/notes/tourist_carrier.py
+++ Graphs/notes/tourist_carrier.py
@@ -9,3 +9,51 @@ amount of people in one group
 
 
 """
+
+from queue import PriorityQueue
+
+def dijkstra(G, s, end):
+    # Initialization:
+    n = len(G)
+    parent = [None for _ in range(n)]
+    d = [0 for _ in range(n)] # min bus capacity (negative to get the biggest abs value)
+    d[s] = -float("inf") # <--- Very important, without this the algorithm won't start
+    visited = [False for _ in range(n)]
+    pq = PriorityQueue()
+    pq.put((d[s], s))
+
+    # Main loop:
+    while(not pq.empty()):
+        prio, vertex = pq.get()
+        if(vertex == end):
+            return parent, abs(prio)
+        if(abs(prio) <= abs(d[vertex])): # We check the vertex only if we haven't already found a better path
+            visited[vertex] = True
+            # Relaxation for the newly found vertex:
+            for (v, length) in G[vertex]:
+                length *= -1
+                if(d[v] > max(d[vertex], length) and visited[v] == False):
+                    d[v] = max(d[vertex], length)
+                    parent[v] = vertex
+                    pq.put((d[v], v))
+    return None
+
+inf = float("inf")
+G=[
+    [(1, 1), (3, 20)],
+    [(0, 1), (2, 0)],
+    [(1,0), 6,20],
+    [(0,20), (4,20)],
+    [(3, 20), (5,20)],
+    [(4,20), (6,20)],
+    [(5,20), (2,20)]
+]
+
+start = 0
+end = 2
+path, cap = dijkstra(G, start, end)
+print(cap)
+while(end != start):
+    print(end, end=" <--- ")
+    end = path[end]
+print(end)
