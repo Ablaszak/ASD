9364541d949c60363b49e0f9a0ea8664077ca130
nottx/PC <a.t.blasiak@gmail.com>
2025-06-11 12:49:41 +0200

    Finished

--- Dynamic/Notes/Cars_on_ferry.py
+++ Dynamic/Notes/Cars_on_ferry.py
@@ -30,17 +30,18 @@ Wywołanie funkcji:
 # Realna implementacja:
 
 def ferry(A, L, i, j, k, f):
+
     if(k == 0):
         return True
 
-    if(f.has_key((i, j, k))): # Co to jest???
+    if((i, j, k) in f): # Już rozpatrywaliśmy?
         return f[(i, j, k)]
 
     f[(i, j, k)] = False
 
-    if(i-A[k] >= 0):
+    if(i-A[k] >= 0): # Można zmieścić na górny pokład
         f[(i, j, k)] |= ferry(A, L, i-A[k], j, k-1, f)
-    if(j-A[k] >=0):                         # |= oznacza "lub równa się"
+    if(j-A[k] >=0): # Można zmieścić na dolny pokład
         f[(i, j, k)] |= ferry(A, L,i, j-A[k], k-1, f)
 
     return f[(i, j, k)]
@@ -62,7 +63,35 @@ def main(A, L):
     return wynik
 
 
+# Moja implementacja:
 
+# top & bot indicates how much space is occupied on top and bottom levels
+def fit(C, size, top, bot, i, dic):
+
+    print("robię ", i, "-ty samochód")
+    if(top == bot == size): # We won't fit anymore
+        return
+
+    if((i, top, bot) in dic): # check if we haven't already calculated this case
+        return
+
+    dic[(i, top, bot)] = True
+
+    if(i < len(C)): # There are still some cars left
+        if(top + C[i] <= size):
+            fit(C, size, top+C[i], bot, i+1, dic)
+        if(bot + C[i] <= size):
+            fit(C, size, top, bot + C[i], i + 1, dic)
+
+cars=[1, 5, 2, 54, 1, 23]
+L = 6
+# Main loop:
+f = dict() # dict says if we can achieve combination: (cars used, top space occupied, bottoms space occupied)
+f[(0, 0, 0)] = True
+fit(cars, L, cars[0], 0, 1, f) # first car goes on top
+fit(cars, L, 0, cars[0], 1, f) # first car goes on bottom
+
+print(max(f))
 
 
 
