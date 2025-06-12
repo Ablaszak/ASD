5944050a3aca93a7602a8640ca3b57eaafb5723f
nottx/PC <a.t.blasiak@gmail.com>
2025-06-12 08:43:42 +0200

    finished, not the best time complexity but I have no time do to it better now

--- Dynamic/Trial_test/kolosy_asd_2022/kol2_b_dynamic/kol2b.py
+++ Dynamic/Trial_test/kolosy_asd_2022/kol2_b_dynamic/kol2b.py
@@ -9,37 +9,23 @@ def min_cost( O, C, T, L ):
     park.sort()
     print(park)
     n = len(park)
-    pq = PriorityQueue()
-    pq_cheat = PriorityQueue()
 
-    # Queue scheme: (summary cost, parking number, did we cheat on the way)
-    pq.put((0, 0, False))
-    #pq_cheat.put((0, 0, False))
-    # Window indexes:
-    right = 0
-    right2 = 0
-    # Main loop:
-    for left in range(n):
-        # Get rid of old stations:
-        while(pq.queue[0][1] < left):
-            pq.get()
-        #while(pq_cheat.queue[0][1] < left):
-        #    pq_cheat.get()
+    # [no cheat, cheat]
+    d = [[float("inf"), float("inf")] for _ in range(n)]
+    d[0][0] = 0
 
-        if(left == n-1): # We got to our destination
-            return pq.get()
+    for start in range(n-1):
+        for stop in range(start+1, n):
+            if(park[stop][0] - park[start][0] <= T):
+                d[stop][0] = min(d[stop][0], d[start][0]+park[stop][1])
+                d[stop][1] = min(d[stop][1], d[start][1]+park[stop][1])
+            elif(park[stop][0] - park[start][0] <= 2*T):
+                d[stop][1] = min(d[stop][1], d[start][0]+park[stop][1])
+            else:
+                break
+    return min(d[n-1][0], d[n-1][1])
 
-        # Widen window:
-        while(right<n and abs(park[right][0] - park[left][0]) <= T):
-            pq.put((pq.queue[0][0] + park[right][1], right, pq.queue[0][2]))
-            pq_cheat.put((pq.queue[0][0] + park[right][1], right, pq.queue[0][2]))
-            right += 1
-        # Widen cheat window:
-        if(pq.queue[0][2] == False):
-            right2 = right
-            while(right2 < n and abs(park[right2][0] - park[left][0]) <= 2*T):
-                pq.put((pq.queue[0][0] + park[right2][1], right2, True))
-                right2 += 1
+
 
 
 Ot = [17, 20, 11, 5, 12]
@@ -49,4 +35,4 @@ Lt = 25
 
 print(min_cost(Ot, Ct, Tt, Lt))
 # zmien all_tests na True zeby uruchomic wszystkie testy
-#runtests( min_cost, all_tests = False )
+runtests( min_cost, all_tests = True )
