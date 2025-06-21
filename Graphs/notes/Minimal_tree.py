a4721b41450f083b338fd65e8429d448a07e19a2
nottx/PC <a.t.blasiak@gmail.com>
2025-06-21 13:34:06 +0200

    idk

--- Graphs/notes/Minimal_tree.py
+++
@@ -1,6 +0,0 @@
-"""
-Find a shortes edge for each vertex
-Merge ever pair of vertexes connected by the same shortes edge (the same edge is shortest for each vertex)
-Repeat
-Somehow in the end we will get a minimal tree
-"""
