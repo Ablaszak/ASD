c82501991b908e1e06c43d883edc93d3af5ad9b2
nottx/PC <a.t.blasiak@gmail.com>
2025-06-22 07:57:14 +0200

    wip, wrote down what to do next

--- Exams/22/egz1/zad_b/egz1b.py
+++ Exams/22/egz1/zad_b/egz1b.py
@@ -2,13 +2,35 @@ from egz1btesty import runtests
 
 class Node:
   def __init__( self ):
-    self.left = None    # lewe poddrzewo
-    self.right = None   # prawe poddrzewo
-    self.x = None       # pole do wykorzystania przez studentow
+        self.left = None
+    self.right = None
+    self.level = None
 
-def wideentall( T ):
-    # tu prosze wpisac wlasna implementacje
+width = []
+# O(n)
+def add_levels(T, lvl=0): # go through the tree and write down level of every node
+    T.level = lvl
+
+    # Update width[]:
+    global width
+    if(len(width) <= lvl):
+        width.append(1)
+    else:
+        width[lvl] += 1
+
+    if(T.right is not None):
+        add_levels(T.right, lvl+1)
+    if(T.left is not None):
+        add_levels(T.left, lvl+1)
+
+def widentall( T ):
+    add_levels(T)
+    global width
+    w = max(width) # O(n)
+
+    # Now I have to check which branches lead to expected width
+    # and cut those that don't
+
     return None
 
-# zmien all_tests na True zeby uruchomic wszystkie testy
-runtests( wideentall, all_tests = False )
+runtests( widentall, all_tests = False )
