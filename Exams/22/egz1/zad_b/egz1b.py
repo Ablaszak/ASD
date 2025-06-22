c839ed52def693d215c8b9ec9fd3520a3323bd7a
Nottx/Laptok <a.t.blasiak@gmail.com>
2025-06-22 11:10:07 +0200

    finished

--- Exams/22/egz1/zad_b/egz1b.py
+++ Exams/22/egz1/zad_b/egz1b.py
@@ -1,10 +1,11 @@
 from egz1btesty import runtests
 
 class Node:
-  def __init__( self ):
+    def __init__( self ):
         self.left = None
-    self.right = None
-    self.level = None
+        self.right = None
+        self.level = None
+        self.furthest = None
 
 width = []
 # O(n)
@@ -18,20 +19,72 @@ def add_levels(T, lvl=0): # go through the tree and write down level of every no
     else:
         width[lvl] += 1
 
+    T.level = lvl
+    T.furthest = lvl
+
     if(T.right is not None):
-        add_levels(T.right, lvl+1)
+        T.furthest = max(T.furthest, add_levels(T.right, lvl+1))
     if(T.left is not None):
-        add_levels(T.left, lvl+1)
+        T.furthest = max(T.furthest, add_levels(T.left, lvl+1))
+
+    return T.furthest
+
+# O(n)
+def count_cuts(T, cutoff):
+
+    # Recursion ends:
+    if(cutoff == T.level): # now we cut off rest of the tree below
+        ctr = 0
+        if(T.left is not None):
+            ctr += 1
+        if(T.right is not None):
+            ctr += 1
+        return ctr
+
+    if(T.furthest < cutoff):
+        return 1 # we cut off this node
+
+    ctr = 0
+    if(T.left is not None):
+        ctr += count_cuts(T.left, cutoff)
+    if(T.right is not None):
+        ctr += count_cuts(T.right, cutoff)
+    return ctr
+
 
 def widentall( T ):
-    add_levels(T)
+
     global width
-    w = max(width) # O(n)
+    width = []
+    add_levels(T)
+
+    # get the level we want to cut off:
+    w = 0
+    lvl = 0
+    for i in range(len(width)): # O(log(n))
+        if(width[i] >= w):
+            w = width[i]
+            lvl = i
+
 
     # Now I have to check which branches lead to expected width
     # and cut those that don't
 
-    
-    return None
-
+    return count_cuts(T, lvl)
+"""
+A = Node()
+B = Node()
+C = Node()
+A.left = B
+A.right = C
+D = Node()
+E = Node()
+B.left = D
+B.right = E
+F = Node()
+E.right = F
+G = Node()
+F.right = G
+"""
+#print(widentall(A))
 runtests( widentall, all_tests = False )
