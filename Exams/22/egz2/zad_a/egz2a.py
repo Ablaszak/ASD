62e60a6242724b978fa3a0b77e454bd3995ae4ce
nottx/PC <a.t.blasiak@gmail.com>
2025-06-23 21:10:38 +0200

    wip, but there is a thunderstorm and I wanna have backup xD

--- Exams/22/egz2/zad_a/egz2a.py
+++ Exams/22/egz2/zad_a/egz2a.py
@@ -2,16 +2,18 @@ from egz2atesty import runtests
 
 class Node:
     def __init__(self):
-        self.range_left = None
-        self.range_right = None
+        self.max_left = None
+        self.max_right = None
         self.max_cap = None
         self.left = None
         self.right = None
+        self.iswarehouse = False
 
 class Warehouse:
     def __init__(self, num, cap):
         self.num = num
         self.cap = cap
+        self.iswarehouse = True
 
 def find_next_two(num):
     two = 2
@@ -20,21 +22,20 @@ def find_next_two(num):
     return two
 
 wnum = 0
+last = None
 
 def build_segment_tree(n, l, r, T, i=0): # n = number of leafs
 
-
     root = Node()
-    root.range_left = l
-    root.range_right = r
-    root.max_cap = T
+    root.max_left = T
+    root.max_right = T
 
     # Recursion end:
-    if (i * i >= n):
+    if (2**(i+1) >= n): # next level should be leaves
         global wnum
         root.left = Warehouse(wnum, T)
         root.right = Warehouse(wnum+1, T)
-        T += 2
+        wnum += 2
         return root
 
     root.left = build_segment_tree(n, l, r//2, T, i+1)
@@ -42,10 +43,32 @@ def build_segment_tree(n, l, r, T, i=0): # n = number of leafs
 
     return root
 
+def put_coal(T, c):
+
+    # Recursion end:
+    if(T.iswarehouse):
+        global last
+        last = T.num
+        T.cap -= c
+
+        return T.cap
+
+    if(T.max_left >= c):
+        T.max_left = put_coal(T.left, c)
+    else:
+        T.max_right = put_coal(T.right, c)
+
+    # This return lets the parent know what is the highest
+    # coal cap in this subtree
+    return max(T.max_left, T.max_right)
+
 def coal( A, T ):
     global wnum
+    global last
     wnum = 0
-    n = len(A)
+    last = None
+    n = find_next_two(len(A))
+
     return -1
 
 runtests( coal, all_tests = False )
