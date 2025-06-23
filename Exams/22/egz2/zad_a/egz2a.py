155cbd9b21a508f87233b45398e0e7b0cc522161
Nottx/Laptok <a.t.blasiak@gmail.com>
2025-06-23 20:30:08 +0200

    wip, almost finished build_tree()

--- Exams/22/egz2/zad_a/egz2a.py
+++ Exams/22/egz2/zad_a/egz2a.py
@@ -1,8 +1,51 @@
 from egz2atesty import runtests
 
+class Node:
+    def __init__(self):
+        self.range_left = None
+        self.range_right = None
+        self.max_cap = None
+        self.left = None
+        self.right = None
+
+class Warehouse:
+    def __init__(self, num, cap):
+        self.num = num
+        self.cap = cap
+
+def find_next_two(num):
+    two = 2
+    while(two < num):
+        two *= 2
+    return two
+
+wnum = 0
+
+def build_segment_tree(n, l, r, T, i=0): # n = number of leafs
+
+
+    root = Node()
+    root.range_left = l
+    root.range_right = r
+    root.max_cap = T
+
+    # Recursion end:
+    if (i * i >= n):
+        global wnum
+        root.left = Warehouse(wnum, T)
+        root.right = Warehouse(wnum+1, T)
+        T += 2
+        return root
+
+    root.left = build_segment_tree(n, l, r//2, T, i+1)
+    root.left = build_segment_tree(n, (r//2) + 1, r, T, i+1)
+
+    return root
+
 def coal( A, T ):
-    # tu prosze wpisac wlasna implementacje
+    global wnum
+    wnum = 0
+    n = len(A)
     return -1
 
-# zmien all_tests na True zeby uruchomic wszystkie testy
 runtests( coal, all_tests = False )
