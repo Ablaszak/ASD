320b44429bd865bb99f1bda5774eb4e75a726513
nottx/PC <a.t.blasiak@gmail.com>
2025-06-25 12:56:03 +0200

    wip, something doesnt work but we are on a good path

--- Exams/22/egz2/zad_b/egz2b.py
+++ Exams/22/egz2/zad_b/egz2b.py
@@ -1,8 +1,31 @@
 from egz2btesty import runtests
 
-def magic( C ):
-    # tu prosze wpisac wlasna implementacje
-    return -1
+def gain(chest, d):
+    if(chest == d[0]):
+        return 0
+    if(chest < d[0]):
+        return chest-d[0]
+    return min(chest-d[0], 10)
 
-# zmien all_tests na True zeby uruchomic wszystkie testy
-runtests( magic, all_tests = False )
+def magic( C ):
+
+    # Init:
+    n = len(C)
+    max_gold = [-1 for _ in range(n)]
+    max_gold[0] = 0
+
+    # Main loop:
+    for current in range(n-1):
+        chest, d0, d1, d2 = C[current]
+        d = [d0,d1,d2]
+        take = min(chest, 10)
+
+        # Try all doors:
+        for i in range(3):
+            if(d[i][1] != -1 and chest-10 <= d[i][0]): # door are openable
+                if(d[i][0] <= chest+max_gold[current]): # we have enough gold to open door
+                    max_gold[d[i][1]] = max(max_gold[d[i][1]], max_gold[current]+gain(chest, d[i]))
+
+    return max_gold[n-1]
+
+runtests( magic, all_tests = True )
