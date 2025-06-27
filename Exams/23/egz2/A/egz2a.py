a4d6cde915e4c76222b96d538ffe44fb4d526f26
nottx/PC <a.t.blasiak@gmail.com>
2025-06-27 21:23:41 +0200

    .

--- Exams/23/egz2/A/egz2a.py
+++
@@ -1,8 +0,0 @@
-from egz2atesty import runtests
-
-def dominance(P):
-  # tu prosze wpisac wlasna implementacje
-  pass
-
-# zmien all_tests na True zeby uruchomic wszystkie testy
-runtests( dominance, all_tests = False )
